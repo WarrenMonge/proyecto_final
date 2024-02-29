ddb60bba76f113ef00bc010ec56306c7b94646d0
WarrenMonge <wmonge50661@ufide.ac.cr>
2024-02-28 21:38:53 -0600

    validar user existente

--- proyecto.py
+++ proyecto.py
@@ -6,10 +6,20 @@ def register():
     password = input("Crear contraseña: ")
     password1 = input("Confirmar contraseña: ")
 
+    u = []
+    p = []                                      #Crea diccionarios para el usuario y la contrasena
+    for i in db:
+        a,b = i.split(", ")                     #se recorre todos los datos del documento de texto para
+        b = b.strip()                           #Validar los usuarios que estan registrados para que no se logre repetir el user
+        u.append(a)                             
+        p.append(b)
+    data = dict(zip(u,p))                       #{'Wawa': '0201', 'Warren': '2022'} Ejemplo de como se recorre, gracias a los diccionarios
+    print(data)
+
     if password != password1:
         print("La contraseñas no son iguales!!")
         register()
-    elif user  in db:
+    elif user in u:
         print("El usuario ya existe!!")
         register()  
 
@@ -17,8 +27,10 @@ def register():
         db = open("login.txt", "a")
         db.write(user + ", " + password + "\n")
         print("Se registro con exito!!")
-        
+
 register()
+
+
 
 
 
