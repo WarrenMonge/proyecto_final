4d78069fc0fe5e5045c99db4f9146f15b9cf8a57
WarrenMonge <wmonge50661@ufide.ac.cr>
2024-02-28 21:23:02 -0600

    creacion de documento de db, and register

--- proyecto.py
+++ proyecto.py
@@ -1,5 +1,24 @@
 #Creacion de ingreso como usuario registrado
 
+def register():
+    db = open("login.txt", "r")
+    user = input("Crear usuario: ")
+    password = input("Crear contraseña: ")
+    password1 = input("Confirmar contraseña: ")
+
+    if password != password1:
+        print("La contraseñas no son iguales!!")
+        register()
+    elif user  in db:
+        print("El usuario ya existe!!")
+        register()  
+
+    else:
+        db = open("login.txt", "a")
+        db.write(user + ", " + password + "\n")
+        print("Se registro con exito!!")
+        
+register()
 
 
 
