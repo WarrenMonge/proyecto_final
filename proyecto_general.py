5fa0ed6448253125321cdd628c20190e5e280ac3
WarrenMonge <wmonge50661@ufide.ac.cr>
2024-04-19 11:24:36 -0600

    arreglo codigo general

--- proyecto_general.py
+++ proyecto_general.py
@@ -14,7 +14,7 @@ price_car = []
 sedes_horarios = {
     1: {"nombre": "San José", "horario": "24 horas, los 7 días de la semana"},
     2: {"nombre": "Alajuela", "horario": "24 horas, los 7 días de la semana"},
-    3: {"nombre": "Guanacaste", "horario": "Abren a las 4 am, cierran a las 11 pm"},   #dicciionario con sus respectivas sedes, para la funcion cambiar de sede 
+    3: {"nombre": "Guanacaste", "horario": "Abren a las 4 am, cierran a las 11 pm"},   # Diccionario que contiene la información sobre las sedes y los horarios en los que operan
     4: {"nombre": "Limón", "horario": "Abren a las 6 am, cierran a las 10 pm"},
     5: {"nombre": "Puntarenas", "horario": "Abren a las 5 am, cierran a las 10 pm"},
     6: {"nombre": "Pérez Zeledón", "horario": "Abren a las 7 am, cierran a las 10 pm"}
@@ -41,7 +41,7 @@ def visualizar_carros():
     modelo_n1 = "kicks"
     modelo_nc1 = 0
     modelo_n2 = "murano"
-    modelo_nc2 = 2
+    modelo_nc2 = 0
     modelo_n3 = "rogue"
     modelo_nc3 = 0
 
@@ -49,7 +49,7 @@ def visualizar_carros():
     modelo_h1 = "santaFe"
     modelo_hc1 = 0
     modelo_h2 = "tucson"
-    modelo_hc2 = 2
+    modelo_hc2 = 0
     modelo_h3 = "venue"
     modelo_hc3 = 0
 
@@ -146,6 +146,54 @@ def visualizar_carros():
             modelo_hc3 -= 1
             print("reservó,", marca3, selec_model)
 
+#Ingreso como invitado
+
+def menu_opciones_invitado():
+    
+    print("Iniciaste sesion correctamente como invitado")
+    print("Bienvenido Usuario invitado" )
+    while True:
+        print("""
+Que te gustaria hacer?             
+
+    (1) Visualizar carros
+    (2) Salir
+                            """)
+        menu_login = int(input("Ingrese su opcion: "))
+        if menu_login == 1:
+            visualizar_carros()
+        elif menu_login ==2:
+            print("Muchas gracias, por elegirnos")
+        break            
+
+
+# Creacion de ingreso como usuario registrado
+def register():
+    db = open("login.txt", "r")
+    user = input("Crear usuario: ") 
+    password = input("Crear contraseña: ")
+    password1 = input("Confirmar contraseña: ")
+
+    u = []
+    p = []                                      # Crea diccionarios para el usuario y la contrasena
+    for i in db:
+        a,b = i.split(",")                     # se recorre todos los datos del documento de texto para
+        b = b.strip()                           # Validar los usuarios que estan registrados para que no se logre repetir el user
+        u.append(a)                             
+        p.append(b)
+    data = dict(zip(u,p))                       # {'Wawa': '0201', 'Warren': '2022'} Ejemplo de como se recorre, gracias a los diccionarios
+
+    if password != password1:
+        print("La contraseñas no son iguales!!")
+        register()
+    elif user in u:
+        print("El usuario ya existe!!")
+        register()  
+
+    else:
+        db = open("login.txt", "a")
+        db.write(user + ", " + password + "\n")
+        print("Se registro con exito!!")
 
 def gestion_inventario():
     print("Gestion de inventario")
@@ -158,10 +206,9 @@ def gestion_inventario():
     while True:
         print("""
     (1) Agregar vehiculos
-    (2) Inhabilitar vehiculo
+    (2) Inhabilite vehiculos
     (3) Estado de vehiculos
-    (4) Cambiar de sede
-    (5) Salir
+    (4) Salir
         """)
         gest_ans = int(input("Ingrese su opcion: "))
         if gest_ans == 1:
@@ -172,27 +219,105 @@ def gestion_inventario():
             price_ad.append(input("Ingrese el precio de alquiler por dia: "))
             price_car.append(input("Ingrese el precio del vehiculo: "))
             state.append("Disponible")
+
         elif gest_ans == 2:
-            print("Indique el carro que desea inhabilitar:")  
-            for i in range(len(marcas)):
-                print(i ,"-", marcas[i], modelos[i], years[i], cc[i], state[i])  #funcion de inabilithar vehiculo, esto para que los usuarios puedan inabilitarlo si asi lo desean 
-            car_index = int(input())
-            state[car_index] = "Inhabilitado"
-            print("El carro ha sido inhabilitado correctamente.")
+            print("Indique la marca del vehiculo que quiera inhabilitar:")  
+            for i in range(len(marca_unique)):
+                print(i ,"-", marca_unique[i]) 
+            marca_search = int(input())
+            print("Cual auto desea inhabilitar:")
+            for x in range(len(modelos)):
+                if marca_unique[marca_search] == marcas[x]:
+                    print(x ,"-", modelos[x],",", years[x],",",cc[x])
+            car_search = int(input())
+            state[car_search] = "Inhabilitado"
         elif gest_ans == 3:
-            for i in range(len(marcas)):
-                print(i ,"-",marcas[i],",", modelos[i],",", years[i],",",cc[i],",",state[i])
+            for y in range(len(modelos)):
+                print(y ,"-",marcas[y],",", modelos[y],",", years[y],",",cc[y],",",state[y])
         elif gest_ans == 4:
-            print("Seleccione la sede a la que desea cambiar:")
-            for key, value in sedes_horarios.items():
-                print(f"{key}. {value['nombre']} - Horario: {value['horario']}")
-            sede_seleccionada = int(input("Ingrese el número correspondiente a la sede deseada: "))
-            print(f"Usted ha seleccionado cambiar a la sede {sedes_horarios[sede_seleccionada]['nombre']}.")  #funcion cambiar sede activa 
-        elif gest_ans == 5:
             break
         else:
             print("Coloca una opcion valida")
 
+def reservacion():
+    print("Reservacion de Autos")
+    while True:
+        print("""
+    (1) Reservar vehiculo
+    (2) Salir
+        """)
+
+def ingreso():
+    db = open("login.txt", "r")
+    user = input("Ingrese su usuario: ")
+    password = input("Ingrese su contraseña: ")
+
+    if not len(user or password) < 1:
+        u = []
+        p = []                                      
+        for i in db:
+            a,b = i.split(",")                     
+            b = b.strip()                           
+            u.append(a)                             
+            p.append(b)
+        data = dict(zip(u,p))
+
+        try:
+            if data[user]:
+                try:
+                    if password == data[user]:
+                        print(f"Ingresaste corectamente {user}")
+                        main_menu()
+                    else:
+                        print("Usuario o contrasena incorrecto!!")
+                except:
+                    print("Contrasena incorrecta del usuario")
+            else:
+                print("El usuario o contrasena no existe!!")
+        except:
+            print("El usuario o contrasena no existe!!!")
+    else:
+        print("Ingrese su datos!!")
+
+
+def cambiar_sede(sede_actual):   # Llama a la función cambiar_sede para cambiar la sede actual, la cual se basa en la informacion del diccionario para poder ejecutar dicha sede.
+    print("Sedes disponibles:")
+    for num, sede_info in sedes_horarios.items():
+        print(f"{num}. {sede_info['nombre']}")
+
+    num_sede = int(input("Ingrese el número de la nueva sede: "))
+
+    if num_sede in sedes_horarios:
+        nueva_sede = sedes_horarios[num_sede]["nombre"]
+        print(f"Sede cambiada a {nueva_sede} correctamente.")
+        return nueva_sede
+    else:
+        print("El número de sede ingresado no es válido.")
+        return sede_actual
+
+def menu():
+    sede_actual = "San José"  #funcion para saber en cual sede se encuentran esto con el fin de hacer que el usuario tenga mas visulacion de la informacion 
+    while True:
+        opc = input("""
+Bienvenido a Fiderents
+                
+    (1) Iniciar Sesion
+    (2) Registrarte
+    (3) Ingreso invitado
+    (4) Salir                   
+                    
+Ingrese tu opcion:""")
+        if opc == "1":
+            ingreso()
+        elif opc ==  "2":
+            register()
+        elif opc == "3":
+            menu_opciones_invitado()
+        elif opc ==  "4":
+            print("Muchas gracias, por elegirnos")
+            break            
+        else:
+            print("Ingrese una de las 4 opciones!!")
 
 def main_menu():
     while True:
@@ -226,4 +351,6 @@ def main_menu():
 
 
 if __name__ == "__main__":
-    main_menu()
+    menu()
+
+
