dda9784aec087d295b64e7bd987390cadde753a1
Erick Portocarrero <dibtecportocarrero@gmail.com>
2024-04-25 22:13:07 -0600

    Arreglo horarios y reserva

--- proyecto_general.py
+++ proyecto_general.py
@@ -20,106 +20,113 @@ sedes_horarios = {
     6: {"nombre": "Pérez Zeledón", "horario": "Abren a las 7 am, cierran a las 10 pm"}
 }
 
-#Proyecto final
-#reservas
+marcas_modelos = {}
+for marca, modelo, year, cc, estado, precio_adicional, precio_auto in zip(marcas, modelos, years, cc, state, price_ad, price_car):
+    if marca not in marcas_modelos:
+        marcas_modelos[marca] = []
+        marca_unique.append(marca)
+    marcas_modelos[marca].append({"modelo": modelo, "year": year, "cc": cc, "estado": estado, "precio_adicional": precio_adicional, "precio_auto": precio_auto})
+    if modelo not in modelo_unique:
+        modelo_unique.append(modelo)
+    if year not in year_unique:
+        year_unique.append(year)
+    if cc not in cc_unique:
+        cc_unique.append(cc)
 
-marca1 = "toyota"
-marca2 = "nissan"
-marca3 = "hyundai"
-
-marcas_modelos = {
-    "toyota": {"yaris": 2, "corolla": 2, "tacoma": 0},
-    "nissan": {"kicks": 0, "murano": 2, "rogue": 0},
-    "hyundai": {"santaFe": 0, "tucson": 2, "venue": 0}
-}
-
-sedes_horarios = {
-    1: {"nombre": "San José", "horario": {"lunes": {"apertura": "00:00", "cierre": "23:59"}, "martes": {"apertura": "00:00", "cierre": "23:59"}, "miércoles": {"apertura": "00:00", "cierre": "23:59"}, "jueves": {"apertura": "00:00", "cierre": "23:59"}, "viernes": {"apertura": "00:00", "cierre": "23:59"}, "sábado": {"apertura": "00:00", "cierre": "23:59"}, "domingo": {"apertura": "00:00", "cierre": "23:59"}}},
-    2: {"nombre": "Alajuela", "horario": {"lunes": {"apertura": "00:00", "cierre": "23:59"}, "martes": {"apertura": "00:00", "cierre": "23:59"}, "miércoles": {"apertura": "00:00", "cierre": "23:59"}, "jueves": {"apertura": "00:00", "cierre": "23:59"}, "viernes": {"apertura": "00:00", "cierre": "23:59"}, "sábado": {"apertura": "00:00", "cierre": "23:59"}, "domingo": {"apertura": "00:00", "cierre": "23:59"}}},
-    3: {"nombre": "Guanacaste", "horario": {"lunes": {"apertura": "04:00", "cierre": "23:00"}, "martes": {"apertura": "04:00", "cierre": "23:00"}, "miércoles": {"apertura": "04:00", "cierre": "23:00"}, "jueves": {"apertura": "04:00", "cierre": "23:00"}, "viernes": {"apertura": "04:00", "cierre": "23:00"}, "sábado": {"apertura": "04:00", "cierre": "23:00"}, "domingo": {"apertura": "04:00", "cierre": "23:00"}}},
-    4: {"nombre": "Limón", "horario": {"lunes": {"apertura": "06:00", "cierre": "22:00"}, "martes": {"apertura": "06:00", "cierre": "22:00"}, "miércoles": {"apertura": "06:00", "cierre": "22:00"}, "jueves": {"apertura": "06:00", "cierre": "22:00"}, "viernes": {"apertura": "06:00", "cierre": "22:00"}, "sábado": {"apertura": "06:00", "cierre": "22:00"}, "domingo": {"apertura": "06:00", "cierre": "22:00"}}},
-    5: {"nombre": "Puntarenas", "horario": {"lunes": {"apertura": "05:00", "cierre": "22:00"}, "martes": {"apertura": "05:00", "cierre": "22:00"}, "miércoles": {"apertura": "05:00", "cierre": "22:00"}, "jueves": {"apertura": "05:00", "cierre": "22:00"}, "viernes": {"apertura": "05:00", "cierre": "22:00"}, "sábado": {"apertura": "05:00", "cierre": "22:00"}, "domingo": {"apertura": "05:00", "cierre": "22:00"}}},
-    6: {"nombre": "Pérez Zeledón", "horario": {"lunes": {"apertura": "07:00", "cierre": "22:00"}, "martes": {"apertura": "07:00", "cierre": "22:00"}, "miércoles": {"apertura": "07:00", "cierre": "22:00"}, "jueves": {"apertura": "07:00", "cierre": "22:00"}, "viernes": {"apertura": "07:00", "cierre": "22:00"}, "sábado": {"apertura": "07:00", "cierre": "22:00"}, "domingo": {"apertura": "07:00", "cierre": "22:00"}}}
-}
 
 def imprimir_disponibilidad(marca):
     if marca in marcas_modelos:
         print(f"Disponibilidad de {marca}:")
         modelos = marcas_modelos[marca]
-        for modelo, cantidad in modelos.items():
-            print(f"{marca}, {modelo}: {cantidad} unidades disponibles")
+        for modelo_info in modelos:
+            modelo = modelo_info["modelo"]
+            estado = modelo_info["estado"]
+            print(f"{marca}, {modelo}: Estado - {estado}")
     else:
         print("Marca no encontrada.")
 
-def verificar_disponibilidad(marcas_modelos, marca_seleccionada, modelo_seleccionado):
+def verificar_disponibilidad(marca_seleccionada, modelo_seleccionado):
     if marca_seleccionada in marcas_modelos:
         modelos = marcas_modelos[marca_seleccionada]
-        if modelo_seleccionado in modelos:
-            cantidad_disponible = modelos[modelo_seleccionado]
-            if cantidad_disponible > 0:
-                print(f"El modelo {modelo_seleccionado} está disponible.")
-                return True
-            else:
-                print(f"Disculpe, el modelo {modelo_seleccionado} no está disponible.")
-        else:
-            print(f"Disculpe, el modelo {modelo_seleccionado} no está disponible para la marca {marca_seleccionada}.")
+        for modelo_info in modelos:
+            if modelo_info["modelo"] == modelo_seleccionado:
+                estado = modelo_info["estado"]
+                if estado == "Disponible":
+                    print(f"El modelo {modelo_seleccionado} de la marca {marca_seleccionada} está disponible.")
+                    return True
+                else:
+                    print(f"Disculpe, el modelo {modelo_seleccionado} de la marca {marca_seleccionada} no está disponible.")
+                    return False
+        print(f"Disculpe, el modelo {modelo_seleccionado} no está disponible para la marca {marca_seleccionada}.")
     else:
         print("Marca no encontrada.")
     return False
 
-def verificar_horario(horario, sede, sedes_horarios):
-    horario_sede = sedes_horarios[sede]["horario"]
-    dia = horario["día"]
-    hora = horario["hora"]
+def verificar_horario(horario, sede_seleccionada):
+    if sede_seleccionada in sedes_horarios:
+        horario_sede = sedes_horarios[sede_seleccionada]["horario"]
+        dia = horario["dia"]
+        hora = horario["hora"]
 
-    if dia.lower() in horario_sede:
-        apertura = horario_sede[dia.lower()]["apertura"]
-        cierre = horario_sede[dia.lower()]["cierre"]
-        if apertura <= hora <= cierre:
-            return True, None
+        if dia.lower() in horario_sede:
+            apertura = horario_sede[dia.lower()]["apertura"]
+            cierre = horario_sede[dia.lower()]["cierre"]
+            if apertura <= hora <= cierre:
+                return True, None
+            else:
+                return False, f"Disculpe, la hora especificada de {horario['tipo']} no es permitida en la sede de {sedes_horarios[sede_seleccionada]['nombre']}. El horario de {dia.capitalize()} es de {apertura} a {cierre}."
         else:
-            return False, f"Disculpe, la hora especificada de {horario['tipo']} no es permitida en la sede de {sedes_horarios[sede]['nombre']}. El horario de {dia.capitalize()} es de {apertura} a {cierre}."
+            return False, f"Disculpe, la sede de {sedes_horarios[sede_seleccionada]['nombre']} no está abierta el {dia.capitalize()}."
     else:
-        return False, f"Disculpe, la sede de {sedes_horarios[sede]['nombre']} no está abierta el {dia.capitalize()}."
+        return False, "Sede no encontrada."
 
-def guardar_reserva(marca, modelo, datos_cliente):
+def guardar_reserva(marca_seleccionada, modelo_seleccionado, datos_cliente):
     with open("reservas.txt", "a") as archivo:
-        archivo.write(f"Marca: {marca}, Modelo: {modelo}\n")
+        archivo.write(f"Marca: {marca_seleccionada}, Modelo: {modelo_seleccionado}\n")
         archivo.write("Datos del cliente:\n")
         for key, value in datos_cliente.items():
             archivo.write(f"{key}: {value}\n")
         archivo.write("\n")
 
 def visualizar_reservas():
-    with open("reservas.txt", "r") as archivo:
-        reservas = archivo.read()
-        print("Reservas realizadas:\n")
-        print(reservas)
+    try:
+        with open("reservas.txt", "r") as archivo:
+            reservas = archivo.read()
+            print("Reservas realizadas:\n")
+            print(reservas)
+    except FileNotFoundError:
+        print("No hay reservas realizadas.")
 
-def hacer_reserva(marcas_modelos, sedes_horarios):
-    marca_seleccionada = input("Ingrese la marca del vehículo: ")
-    imprimir_disponibilidad(marca_seleccionada)
-    modelo_seleccionado = input("Ingrese el modelo del vehículo: ")
-    if verificar_disponibilidad(marcas_modelos, marca_seleccionada, modelo_seleccionado):
-        marcas_modelos[marca_seleccionada][modelo_seleccionado] -= 1
-        print("Reserva realizada con éxito.")
-        datos_cliente = {}
-        datos_cliente["Nombre"] = input("Ingrese su nombre: ")
-        
-        datos_cliente["Teléfono"] = input("Ingrese su número de teléfono: ")
-        guardar_reserva(marca_seleccionada, modelo_seleccionado, datos_cliente)
+def hacer_reserva():
+    for marca in marcas:
+        if marca not in marca_unique:
+            marca_unique.append(marca)  
+    print("Ingrese la marca del vehículo: ")
+    for i in range(len(marca_unique)):
+        print(i ,"-", marca_unique[i])
+    marca_seleccionada = int(input())
+    if marcas[marca_seleccionada] in marcas_modelos:
+        imprimir_disponibilidad(marcas[marca_seleccionada])
+        modelo_seleccionado = input("Ingrese el modelo del vehículo: ").lower()
+        if verificar_disponibilidad(marcas[marca_seleccionada], modelo_seleccionado):
+            datos_cliente = {}
+            datos_cliente["Nombre"] = input("Ingrese su nombre: ")
+            datos_cliente["Teléfono"] = input("Ingrese su número de teléfono: ")
+            guardar_reserva(marcas[marca_seleccionada], modelo_seleccionado, datos_cliente)
+        else:
+            print("No se pudo realizar la reserva.")
     else:
-        print("No se pudo realizar la reserva.")
+        print("No disponibilidad.")
 
-def mostrar_menu_horario(marcas_modelos, sedes_horarios):
+def mostrar_menu_horario():
     print("Seleccione la marca y modelo de vehículo que desea rentar:")
     for marca, modelos in marcas_modelos.items():
-        print(f"{marca}: {', '.join(modelos.keys())}")
+        print(f"{marca}: {', '.join([modelo_info['modelo'] for modelo_info in modelos])}")
 
-    marca_seleccionada = input("Ingrese la marca del vehículo: ")
+    marca_seleccionada = input("Ingrese la marca del vehículo: ").lower()
     if marca_seleccionada in marcas_modelos:
-        modelo_seleccionado = input("Ingrese el modelo de vehículo que desea rentar: ")
-        if modelo_seleccionado in marcas_modelos[marca_seleccionada]:
+        modelo_seleccionado = input("Ingrese el modelo de vehículo que desea rentar: ").lower()
+        if modelo_seleccionado in [modelo_info['modelo'] for modelo_info in marcas_modelos[marca_seleccionada]]:
             print(f"Ha seleccionado {marca_seleccionada} {modelo_seleccionado}.")
 
     print("1 - San José, horario 24 horas, los 7 días de la semana")
@@ -135,8 +142,8 @@ def mostrar_menu_horario(marcas_modelos, sedes_horarios):
     dia_entrega = input("Día de entrega: ").lower()
     hora_entrega = input("Hora de entrega (HH:MM): ")
 
-    retiro_valido, retiro_error = verificar_horario({"día": dia_retiro, "hora": hora_retiro, "tipo": "retiro"}, sede_seleccionada, sedes_horarios)
-    entrega_valida, entrega_error = verificar_horario({"día": dia_entrega, "hora": hora_entrega, "tipo": "entrega"}, sede_seleccionada, sedes_horarios)
+    retiro_valido, retiro_error = verificar_horario({"dia": dia_retiro, "hora": hora_retiro, "tipo": "retiro"}, sede_seleccionada)
+    entrega_valida, entrega_error = verificar_horario({"dia": dia_entrega, "hora": hora_entrega, "tipo": "entrega"}, sede_seleccionada)
 
     if not retiro_valido:
         print(retiro_error)
@@ -147,9 +154,7 @@ def mostrar_menu_horario(marcas_modelos, sedes_horarios):
     else:
         print("Horarios de retiro y entrega válidos. Proceda con la reserva.")
 
-# DATOS COLOCADOS ACA SOLAMENTE SON PRUEBAS
 def visualizar_carros():
-    # Proyecto final
     # Vizualizar vehiculos
     for marca in marcas:
         if marca not in marca_unique:
@@ -163,8 +168,6 @@ def visualizar_carros():
         if marca_unique[marca_search] == marcas[x]:
             print(x ,"-", modelos[x],",", years[x],",",cc[x],",",state[x])
 
-    
-  
 #Ingreso como invitado
 
 def menu_opciones_invitado():
@@ -184,7 +187,6 @@ Que te gustaria hacer?
         elif menu_login ==2:
             print("Muchas gracias, por elegirnos")
         break            
-
 
 # Creacion de ingreso como usuario registrado
 def register():
@@ -296,7 +298,6 @@ def ingreso():
     else:
         print("Ingrese sus datos!!")
 
-
 def cambiar_sede(sede_actual):   # Llama a la función cambiar_sede para cambiar la sede actual, la cual se basa en la informacion del diccionario para poder ejecutar dicha sede.
     print("Sedes disponibles:")
     for num, sede_info in sedes_horarios.items():
@@ -350,9 +351,7 @@ def main_menu():
         if ans == 1:
             visualizar_carros()
         elif ans == 2:
-            mostrar_menu_horario(marcas_modelos, sedes_horarios)
-            hacer_reserva(marcas_modelos, sedes_horarios)
-            visualizar_reservas()
+            hacer_reserva()
         elif ans == 3:
             print("Entrega realizada correctamente")
         elif ans == 4:
@@ -367,7 +366,6 @@ def main_menu():
             break
         else:
             print("Coloca una opcion valida")
-
 
 if __name__ == "__main__":
     menu()
